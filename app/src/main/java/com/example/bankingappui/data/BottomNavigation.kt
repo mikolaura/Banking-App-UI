1a8783dbe9914d5478b0b84a649777424e33cb0a
unknown <mikolaura58@gmail.com>
2023-12-21 17:10:30 +0200

    12 06

---
+++ app/src/main/java/com/example/bankingappui/data/BottomNavigation.kt
@@ -0,0 +1,6 @@
+package com.example.bankingappui.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigation(val title: String,
+                            val icon: ImageVector)
--- app/src/main/java/com/example/bankingappui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/bankingappui/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun BankingAppUITheme(
+fun BankningAppUITheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
